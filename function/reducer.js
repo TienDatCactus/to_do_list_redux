5d04a9145810387e330ac4d9b1b0ad154dbc7e1f
tien dat <multivncraft@gmail.com>
2024-04-27 20:59:26 +0700

    added local storage utility functions

--- function/reducer.js
+++ function/reducer.js
@@ -1,20 +1,37 @@
-
 const init = {
-  todos:[
+  todos: [
     {
-      title : 'Nigga',
-      completed : false
+      title: "Nigga",
+      completed: false,
     },
     {
-      title : 'Chigga',
-      completed : true
-    }
-  ]
+      title: "Chigga",
+      completed: true,
+    },
+  ],
+};
+
+const actions = {
+  ADD({ todos }, title) {
+    todos.push({ title, completed: false });
+  },
 };
 
 export default function reducer(state = init, action, args) {
-  switch (action) {
-    default:
-      return state;
-  }
+  actions[action] && actions[action](state, ...args);
+  return state;
+  // switch (action) {
+  //   case "ADD":
+  //     const [title] = args;
+  //     return {
+  //       ...state,
+  //       todos: [
+  //         ...state.todos,
+  //         {
+  //           title,
+  //           completed: false,
+  //         },
+  //       ],
+  //     };
+  //   default:
 }
